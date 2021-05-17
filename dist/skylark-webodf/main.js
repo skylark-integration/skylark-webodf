5ef566e54534c72d1c1b277dcb2a683b4a133a99
hudaokeji <github@hudaokeji.com>
2021-05-17 09:54:04 +0900

    init commit

---
+++ dist/skylark-webodf/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-webodf - A version of webodf.js that ported to running on skylarkjs.
+ * @author Hudaokeji Co.,Ltd
+ * @version v0.9.0
+ * @link www.skylarkjs.org
+ * @license MIT
+ */
+define(["skylark-langx-ns","./webodf"],function(n,e){return n.attach("intg.webodf",e)});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ src/main.js
@@ -0,0 +1,6 @@
+define([
+	"skylark-langx-ns",
+	"./webodf"
+],function(skylark,webodf) {
+	return skylark.attach("intg.webodf",webodf)
+});
